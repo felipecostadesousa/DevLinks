9997b446da122149dc299046a1e63bd435357e9d
felipecostadesousa <felipecostadesousa.sigma@gmail.com>
2023-08-24 09:09:42 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMode(){
+    const html = document.documentElement
+    html.classList.toggle("light")
+    const image = document.querySelector("#profile img")
+    if(html.classList.contains("light")){
+        image.setAttribute("src", "./assets/assets/avatar-light.png")
+    } else {
+        image.setAttribute("src", "./assets/assets/avatar.png")
+    }
+}
